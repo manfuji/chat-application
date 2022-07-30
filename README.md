7db22d889c74a31c1efeba0625f946e81611b67c
manfuji <telebryt@gmail.com>
2022-07-30 22:02:34 +0000

    Made changes to the Ui

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Next.js + Tailwind CSS Example
+# Next.js + Tailwind CSS + SocketIo Application
 
 The project is styles with [Tailwind CSS](https://tailwindcss.com/) [(v3.0)](https://tailwindcss.com/blog/tailwindcss-v3) with Next.js. It follows the steps outlined in the official [Tailwind docs](https://tailwindcss.com/docs/guides/nextjs).
 
@@ -12,11 +12,11 @@ Deploy to vercel instead of heroku because of the edge functions used in the pro
 
 Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with [npm](https://docs.npmjs.com/cli/init), [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/), or [pnpm](https://pnpm.io) to bootstrap the example:
 
+used SocketIo to provide the live Communication
+
 Socket Server Hosted on Heroku [heroku cli](https://fujisocket.herokuapp.com)
 
 # socketServer
-
-# chat-application
 
 # chat-application
 
--- pages/chatPage.jsx
+++ pages/chatPage.jsx
@@ -239,12 +239,14 @@ const ChatApp = () => {
                   onChange={(e) => setSendMessage(e.target.value)}
                   className="w-full mx-auto px-2 py-3 outline-none rounded-lg text-gray-700"
                 />
-                <button
-                  onClick={handleSubmit}
-                  className="lg:text-base text-sm md:h-12 h-8 tracking-widest pb-2 uppercase text-blue-700 shadow-lg shadow-gray-400 md:w-64 w-44 bg-slate-20 bg-slate-100 pt-2.5 mt-5 ring-1 rounded-xl px-4 ring-gray-200 text-center justify-center items-center"
-                >
-                  Send message
-                </button>
+                {sendMessage !== '' && (
+                  <button
+                    onClick={handleSubmit}
+                    className="lg:text-base text-sm md:h-12 h-8 tracking-widest pb-2 uppercase text-blue-700 shadow-lg shadow-gray-400 md:w-64 w-44 bg-slate-20 bg-slate-100 pt-2.5 mt-5 ring-1 rounded-xl px-4 ring-gray-200 text-center justify-center items-center"
+                  >
+                    Send message
+                  </button>
+                )}
               </form>
             </div>
           </div>
